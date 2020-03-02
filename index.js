cdc060a70ed1467ac840f23429bbdf413d160513
Miles Higbie <miles.higbie@gmail.com>
2020-03-02 01:33:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ class Scooter {
 }
 
 class Driver {
-  
+  constructor(name,age,experience)
+    this.name = year  
+    this.model = model
+    this.color = color
 }
+expect(allison).to.be.an.instanceof(Driver);
+      expect(allison.name).to.equal("Allison");
+      expect(allison.age).to.equal("16 years");
+      expect(allison.experience).to.equal("2 years");
